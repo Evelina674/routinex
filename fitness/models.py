9e2139de06e5305f39c62cbe1ecd391baa294c81
nasedry <150812942+nasedry@users.noreply.github.com>
2026-04-27 18:41:40 +0300

    Зроблено все мінімально

--- fitness/models.py
+++ fitness/models.py
@@ -1,41 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     weight = models.FloatField()
     height = models.FloatField()
     goal = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.email
+        return self.user.username
+
 
 class Workout(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    is_public = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Exercise(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     sets = models.IntegerField()
     reps = models.IntegerField()
-    weight = models.FloatField()
+
 
 class Meal(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     calories = models.IntegerField()
-    date = models.DateTimeField()
+
 
 class Habit(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    is_completed = models.BooleanField(default=False)
-    date = models.DateField()
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+
 
 class Progress(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    weight_record = models.FloatField()
-    date = models.DateField()
+    weight = models.FloatField()
+    date = models.DateField(auto_now_add=True)
--- fitness/views.py
+++ fitness/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserProfile, Workout, Meal, Habit, Progress
 
-# Create your views here.
+def dashboard(request):
+   return render(request, "fitness/dashboard.html")
+
+def add_workout(request):
+    if request.method == "POST":
+        Workout.objects.create(
+            user=UserProfile.objects.first(),
+            title=request.POST.get("title")
+        )
+    return redirect("/")
+
+def add_meal(request):
+    if request.method == "POST":
+        Meal.objects.create(
+            user=UserProfile.objects.first(),
+            name=request.POST.get("name"),
+            calories=request.POST.get("calories")
+        )
+    return redirect("/")
+
+def add_habit(request):
+    if request.method == "POST":
+        Habit.objects.create(
+            user=UserProfile.objects.first(),
+            title=request.POST.get("title")
+        )
+    return redirect("/")
+
+def add_progress(request):
+    if request.method == "POST":
+        Progress.objects.create(
+            user=UserProfile.objects.first(),
+            weight=request.POST.get("weight")
+        )
+    return redirect("/")
